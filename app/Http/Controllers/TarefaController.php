8eebde3e9250e64e99658d71ee8a397058b14565
IsabellaMotaC <isabellamotacarvalho@hotmail.com>
2025-03-20 09:21:59 -0300

    adicionado migration, model e controller da Tarefa

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TarefaController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(string $id)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+    }
+}
